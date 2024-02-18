81a6adcd368a743976de88e4f105eeb4c7e6fdd3
shakil <kazimuhammadullah@gmail.com>
2024-02-18 19:05:19 +0600

    lokomoko design

--- resources/views/frontend/return_policy.blade.php
+++ resources/views/frontend/return_policy.blade.php
@@ -2,14 +2,105 @@
 @extends('frontend.layouts.master')
 @section('content')
     
-<h1>Coming Soon...</h1>
+
+<section class="banner d-flex align-items-center" style="background-image: url('{{ asset('product/about.jpg')}}');">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center flex-column d-flex align-items-center justify-content-center">
+                <div class="box text-uppercase fw-bold d-flex align-items-center fs-6">
+                    <small><a href="{{route('homepage')}}" class="text-white text-decoration-none  fs-6 hover-primary">Home</a></small>
+                    <iconify-icon icon="fluent:chevron-right-24-filled" class="text-white mx-2" width="1.2em"
+                        height="1.2em"></iconify-icon>
+                    <small class="text-white fs-6">Return Policy</small>
+                </div>
+                <h1 class="statliches display-1 text-light">Return Policy</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="py-5">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-lg-8 text-justify">
+                <h1 class="statliches mb-4">Return Policy</h1>
+
+                {{-- <h5 class="statliches mb-4">A BRIGHT IDEA TAKES PLACE</h5> --}}
+
+                
+                <p>Thank you for your purchase. We hope you are happy with your purchase. However, if you are not completely satisfied with your purchase for any reason, you may return it to us for a full refund. Please see below for more information on our return policy.</p>
+
+
+                
+                
+                            
+                <p>All returns must be postmarked within 30 days of the purchase date. All returned items must be in new and unused condition, with all original tags and labels attached.</p>
+                
+                
+                
+                <div><h3 class="">RETURN PROCESS</h3></div>
+                
+                
+                
+                <p>To return an item, please e mail support@littcocktails,com to obtain a Return Merchandise Authorization (RMA) number. After receiving a RMA number, place the item securely in its original packaging and [include your proof of purchase / the return form provided / other], and mail your return to the following address:</p>
+                
+                
+                
+                <p>Katwa Globe (OPC) Pvt Ltd</p>
+                
+                
+                
+                <h3>Attn: Returns</h3>
+                
+                
+                
+                <p>Katwa Globe (OPC) Pvt. Ltd. Katwa Crystal, Plot No 782, 7th Cross Bhagyanagar, Belgaum – 590006<br></p>
+                
+                
+                
+                <p>Please note, you will be responsible for all return shipping charges. We strongly recommend that you use a trackable method to mail your return. / You may also use the prepaid shipping label enclosed with your package.</p>
+                
+                
+                
+                <h3>REFUNDS</h3>
+                
+                
+                
+                <p>After receiving your return and inspecting the condition of your item, we will process your refund. Please allow at least 7 days from the receipt of your item to process your refund. Refunds may take 1-2 billing cycles to appear on your credit card statement, depending on your credit card company. We will notify you by email when your return has been processed.</p>
+                
+                
+                
+                <h3 class="stk-block-heading__text">QUESTIONS</h3>
+                
+                
+                
+                <p>If you have any questions concerning our return policy, please contact us at:</p>
+                
+                
+                
+                <p>+91-733-850-9404</p>
+                
+                
+                
+                <p>support@cocktails.com</p>
+                </div>
+
+
+
+
+                
+            </div>
+        </div>
+    </div>
+</section>
 
 
 @endsection
-
-
 @section('script')
     
 @endsection
+
+
+
 
 
--- resources/views/frontend/shipping_policy.blade.php
+++ resources/views/frontend/shipping_policy.blade.php
@@ -2,14 +2,93 @@
 @extends('frontend.layouts.master')
 @section('content')
     
-<h1>Coming Soon...</h1>
+
+<section class="banner d-flex align-items-center" style="background-image: url('{{ asset('product/about.jpg')}}');">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center flex-column d-flex align-items-center justify-content-center">
+                <div class="box text-uppercase fw-bold d-flex align-items-center fs-6">
+                    <small><a href="{{route('homepage')}}" class="text-white text-decoration-none  fs-6 hover-primary">Home</a></small>
+                    <iconify-icon icon="fluent:chevron-right-24-filled" class="text-white mx-2" width="1.2em"
+                        height="1.2em"></iconify-icon>
+                    <small class="text-white fs-6">Shipping Policy</small>
+                </div>
+                <h1 class="statliches display-1 text-light">Shipping Policy</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="py-5">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-lg-8 text-justify">
+                <h1 class="statliches mb-4">Shipping Policy</h1>
+
+                {{-- <h5 class="statliches mb-4">A BRIGHT IDEA TAKES PLACE</h5> --}}
+
+                  
+    <h3 class="stk-block-heading__text">What are the Shipping Charges?</h3>
+    
+    
+    
+    <p>Shipping charges are decided by our logistic partners like Amazon etc.</p>
+    
+    
+    
+    <h3>How Long does it take&nbsp;for an Order to get Shipped?</h3>
+    
+    
+    
+    <p>All orders get shipped within 3-5 working days and you can expect the delivery in 5-7 working days to reach in all cities in India.</p>
+    
+    
+    
+    <h3>How Long does&nbsp;it take&nbsp;for an Order to get Delivered?</h3>
+    
+    
+    
+    <p>You can expect the delivery in 5-7 working days to reach in all parts of India.</p>
+    
+    
+    
+    <h3>How to Track the Status of my Order?</h3>
+    
+    
+    
+    <p>You will receive the tracking link in your email as soon as your order is dispatched. It takes 12 to 24hrs for the status of the shipment to update on the tracking link.</p>
+    
+    
+    
+    <h3>How to change an incorrect shipping address?</h3>
+    
+    
+    
+    <p>If you have entered a wrong or incorrect shipping address, and if your order has not been dispatched then you can update us with your correct shipping address immediately by emailing us at support@littcocktails.com</p>
+    
+    
+    
+    <h3>What is your Return Policy?</h3>
+    
+    
+    
+    <p>All shipping and handling charges are non-refundable. All returns made should be aligned with our refund policies and guidelines.</p>
+
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 
 @endsection
-
-
 @section('script')
     
 @endsection
+
+
+
 
 
